e494b257ba888df6201c149c39be9bcfef5609b2
Diego <diego.lavayen@digitalbridge.com>
2021-04-08 09:52:24 +0100

    feat: add propTypes to post-entry and post-list components

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -10,4 +10,40 @@ const PostList = ({ posts, users }) => {
     });
 };
 
+PostList.propTypes = {
+    posts: PropTypes.arrayOf(
+        PropTypes.shape({
+            userId: PropTypes.number,
+            id: PropTypes.number,
+            title: PropTypes.string,
+            body: PropTypes.string,
+        }),
+    ).isRequired,
+    users: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            name: PropTypes.string,
+            username: PropTypes.string,
+            email: PropTypes.string,
+            address: PropTypes.shape({
+                street: PropTypes.string,
+                suite: PropTypes.string,
+                city: PropTypes.string,
+                zipcode: PropTypes.string,
+                geo: PropTypes.shape({
+                    lat: PropTypes.string,
+                    lng: PropTypes.string,
+                }),
+            }),
+            phone: PropTypes.string,
+            website: PropTypes.string,
+            company: PropTypes.shape({
+                name: PropTypes.string,
+                catchPhrase: PropTypes.string,
+                bs: PropTypes.string,
+            }),
+        }),
+    ).isRequired,
+};
+
 export default PostList;
