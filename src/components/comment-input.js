f1d0495c6c5bb8495ee440f5027ef42fe304a372
Diego <diego.lavayen@digitalbridge.com>
2021-04-08 10:34:19 +0100

    fix: update comment list with textarea content

--- src/components/comment-input.js
+++ src/components/comment-input.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CommentInput = ({ addComment }) => {
+    const [input, setInput] = useState('');
+
+    const updateComment = value => {
+        setInput(value);
+    };
+
     return (
         <>
             <div>
-                <textarea placeholder="comment" />
+                <textarea
+                    value={input}
+                    onChange={event => updateComment(event.target.value)}
+                    placeholder="comment"
+                />
             </div>
             <div>
                 <button
                     type="button"
                     onClick={() => {
-                        addComment('comment');
+                        addComment(input);
+                        setInput('');
                     }}
                 >
                     submit
