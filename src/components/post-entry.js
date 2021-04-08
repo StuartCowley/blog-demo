173768288d5edf358613fcea80635e901454bdc4
Diego <diego.lavayen@digitalbridge.com>
2021-04-08 10:06:51 +0100

    feat: introducing likeCounter state in PostEntry component

--- src/components/post-entry.js
+++ src/components/post-entry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // types
 import { PostType } from '../types/post.type';
@@ -7,11 +7,23 @@ import { UserType } from '../types/user.type';
 const PostEntry = ({ post, user }) => {
     const { title, body } = post;
     const { name } = user;
+
+    const [likeCounter, setLikeCounter] = useState(0);
+
+    const updateCounter = () => {
+        setLikeCounter(previousCounter => previousCounter + 1);
+    };
     return (
         <article>
             <h2>{title}</h2>
             <h3>by {name}</h3>
             <p>{body}</p>
+            <div>
+                <span>{likeCounter}</span>
+                <button type="button" onClick={() => updateCounter()}>
+                    +
+                </button>
+            </div>
         </article>
     );
 };
