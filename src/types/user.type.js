3cc7338c2bea1b4ee5617168eb16953d544b3303
Diego <diego.lavayen@digitalbridge.com>
2021-04-08 10:01:42 +0100

    feat: extracting further complex types

--- src/types/user.type.js
+++ src/types/user.type.js
@@ -1,25 +1,31 @@
 import PropTypes from 'prop-types';
 
+const GeoLocalisationType = PropTypes.shape({
+    lat: PropTypes.string,
+    lng: PropTypes.string,
+});
+
+const AddressType = PropTypes.shape({
+    street: PropTypes.string,
+    suite: PropTypes.string,
+    city: PropTypes.string,
+    zipcode: PropTypes.string,
+    geo: GeoLocalisationType,
+});
+
+const CompanyType = PropTypes.shape({
+    name: PropTypes.string,
+    catchPhrase: PropTypes.string,
+    bs: PropTypes.string,
+});
+
 export const UserType = PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     username: PropTypes.string,
     email: PropTypes.string,
-    address: PropTypes.shape({
-        street: PropTypes.string,
-        suite: PropTypes.string,
-        city: PropTypes.string,
-        zipcode: PropTypes.string,
-        geo: PropTypes.shape({
-            lat: PropTypes.string,
-            lng: PropTypes.string,
-        }),
-    }),
+    address: AddressType,
     phone: PropTypes.string,
     website: PropTypes.string,
-    company: PropTypes.shape({
-        name: PropTypes.string,
-        catchPhrase: PropTypes.string,
-        bs: PropTypes.string,
-    }),
+    company: CompanyType,
 });
