c717366f4842b83af45661e80565b47f8069f417
StuartCowley <voidstalker@live.co.uk>
2021-04-27 22:01:21 +0100

    added plain link to post summary

--- src/components/PostEntrySummary.js
+++ src/components/PostEntrySummary.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import PropTypes, { string } from 'prop-types';
 
 // types
 import { PostType } from '../types/post.type';
@@ -9,7 +8,9 @@ import { UserType } from '../types/user.type';
 // components
 import LikeCounter from './LikeCounter';
 
-const PostEntrySummary = ({ post, user, children }) => {
+import { PlainLink } from '../styles/GlobalStyles';
+
+const PostEntrySummary = ({ post, user, theme, children }) => {
     const { title, body } = post;
     const { name } = user;
 
@@ -26,7 +27,9 @@ const PostEntrySummary = ({ post, user, children }) => {
     return (
         <article>
             <h2>
-                <Link to={`post/${post.id}`}>{title}</Link>
+                <PlainLink theme={theme} to={`post/${post.id}`}>
+                    {title}
+                </PlainLink>
             </h2>
             <h3>by {name}</h3>
             <p>{body}</p>
@@ -45,6 +48,7 @@ const PostEntrySummary = ({ post, user, children }) => {
 PostEntrySummary.propTypes = {
     post: PostType.isRequired,
     user: UserType.isRequired,
+    theme: string.isRequired,
     children: PropTypes.node,
 };
 
